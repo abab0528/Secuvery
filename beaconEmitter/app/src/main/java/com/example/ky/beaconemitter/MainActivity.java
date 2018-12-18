655c127ea03bb49d65da2b5e3670fb41f7f0ff13
abab0528 <abab0528@naver.com>
2018-12-18 21:58:46 +0900

    upload

---
+++ beaconEmitter/app/src/main/java/com/example/ky/beaconemitter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ky.beaconemitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("9")
+                .setManufacturer(0x004C)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon);
+    }
+}
